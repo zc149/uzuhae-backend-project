756a5021564e154258458e9d75fd400a17966236
knh9612 <knh9612@naver.com>
2024-04-12 11:21:43 +0900

    Refactor macdduck 패키지 구조에 맞게 코드 리팩

--- local/src/main/java/project/local/service/UserImpl.java
+++
@@ -1,9 +0,0 @@
-package project.local.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class UserImpl {
-}
