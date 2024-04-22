d97bb98aa836de92a9111c4be8c9f572ae89b6c0
macdduck <151634964+knh9612@users.noreply.github.com>
2024-04-22 17:10:30 +0900

    Refac macdduck another conflicts at config

--- local/src/main/java/project/local/config/SecurityConfig.java
+++ local/src/main/java/project/local/config/SecurityConfig.java
@@ -61,13 +61,9 @@ public class SecurityConfig {
         //                 .anyRequest().authenticated());
 
         http
-<<<<<<< HEAD
+
                 .authorizeHttpRequests((auth) -> auth
                         .antMatchers("/**").permitAll());
-=======
-                    .authorizeHttpRequests((auth) -> auth
-                            .antMatchers("/**").permitAll());
->>>>>>> d122f3abc6c03d65a55558f9d46dd9f48a36c77c
 
         http
                 .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
